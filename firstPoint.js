893f1b697213c75a6cc8ce9b6768b34a761ef984
poojalandejosh <pooja.lande@joshsoftware.com>
2024-03-18 19:09:13 +0530

    learned variable declaration

---
+++ firstPoint.js
@@ -0,0 +1,33 @@
+let arr = [1,2,3,4]
+// push method 
+arr.push(9)
+console.log(arr)
+
+// pop method 
+arr.pop()
+console.log(arr)
+
+// shift method 
+arr.shift()
+console.log(arr)
+
+// unShift method 
+arr.unshift(1)
+console.log(arr)
+
+const arr1 = [1,2,3,4,11,23,5]
+// push method 
+arr1.push(9)
+console.log(arr)
+
+// pop method 
+arr1.pop()
+console.log(arr)
+
+// shift method 
+arr1.shift()
+console.log(arr)
+
+// unShift method 
+arr1.unshift(1)
+console.log(arr)
---
+++ secondPoint.js
@@ -0,0 +1,15 @@
+const person = {
+  firstName: "abc",
+  lastName: "xyz",
+  id: 55,
+};
+person.firstName = "lllllll";
+console.log('object is ..',person)
+
+let person = {
+    firstName: "abc",
+    lastName: "xyz",
+    id: 55,
+  };
+  person.firstName = "lllllll";
+  console.log('object is ..',person)
